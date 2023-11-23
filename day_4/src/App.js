a9070db8f001cfe48034f21f2422ca59bb841bbb
Sudhaveni Bhargavi <sudhavenibhargavi207@gmail.com>
2023-11-23 15:14:12 +0530

    App.js file modified

--- day_4/src/App.js
+++ day_4/src/App.js
@@ -1,9 +1,8 @@
 
 import './App.css';
-import AgePredictor from './Components/AgePredictor';
+
 import Excuse from './Components/Excuse';
-import { FetchExample } from './Components/FetchExample';
-import { Show } from './Components/Show';
+
 
 function App() {
   return (
